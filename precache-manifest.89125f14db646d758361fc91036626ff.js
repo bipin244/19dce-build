b7c920049d91496a69560a8e5054ad57157d9f93
bipin patel <bpnptl24@gmail.com>
2020-05-04 11:20:33 +0530

    first commit

---
+++ precache-manifest.89125f14db646d758361fc91036626ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1922f8faa660164f47ab85287c09b55f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e5719494481a79fe7d",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b7061866e6b8a57a4f30",
+    "url": "/static/css/main.649e8446.chunk.css"
+  },
+  {
+    "revision": "d2e5719494481a79fe7d",
+    "url": "/static/js/2.51500b74.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/static/js/2.51500b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7061866e6b8a57a4f30",
+    "url": "/static/js/main.eb480082.chunk.js"
+  },
+  {
+    "revision": "56571aafec9d31dd921e",
+    "url": "/static/js/runtime-main.fca616f6.js"
+  }
+]);
